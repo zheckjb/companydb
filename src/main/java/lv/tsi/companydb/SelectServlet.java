8778e99223a0c646467dfa4826b950367ec30b26
zheckjb <zheck@inbox.lv>
2018-03-01 20:52:04 +0200

    Lesson 7

--- src/main/java/lv/tsi/companydb/SelectServlet.java
+++ src/main/java/lv/tsi/companydb/SelectServlet.java
@@ -9,28 +9,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.sql.*;
 
 @WebServlet(name = "SelectServlet", urlPatterns = "/select")
 public class SelectServlet extends HttpServlet {
     private static final Logger logger = Logger.getLogger(SelectServlet.class);
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Post method");
+        String fullName = request.getParameter("fullName");
+        logger.info(fullName);
+        try (Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/company");
+             //Statement stmt = connection.createStatement();
+             PreparedStatement stmt = connection.prepareStatement("SELECT * FROM PERSON WHERE FULLNAME = ?");
 
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try (
-                Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/company");
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM POSITION ");
+             PrintWriter out = response.getWriter()
         ) {
+            stmt.setString(1,fullName);
+            ResultSet rs = stmt.executeQuery();
+            out.format("| %5s | %-30s | %15s | %10s | %10s |\n", "ID", "NAME", "SALARY", "POSITION", "DEPARTMENT");
             while (rs.next()) {
                 long id = rs.getLong("ID");
-                String name = rs.getString("Name");
-                logger.info("ID: "+id + "; Name: "+name);
-           }
-        }  catch (SQLException e) {
-            logger.error("Something wrong!");
+                String name = rs.getString("FULLNAME");
+                long positionId = rs.getLong("POSITION_ID");
+                long departmentId = rs.getLong("DEPARTMENT_ID");
+                BigDecimal salary = rs.getBigDecimal("SALARY");
+                out.format("| %5d | %-30s | %15.2f | %10d | %10d |\n", id, name, salary, positionId, departmentId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,13 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Comapny DB</title>
 </head>
 <body>
 <p>Welcome!</p>
+<form method="post" action="select">
+    <input type="text" name="fullName" value="">
+    <button type="submit">Select</button>
+</form>
 </body>
 </html>
